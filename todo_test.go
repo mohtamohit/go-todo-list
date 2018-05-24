15c71bdcf5baaf49f9aa81cdb82ffa223008243a
Mohit Mohta <mohtamohit@gmail.com>
2018-05-24 16:23:32 +0530

    Added basic tests

---
+++ todo_test.go
@@ -0,0 +1,19 @@
+package todo
+
+import "testing"
+
+func TestCreate(t *testing.T) {
+
+}
+
+func TestRead(t *testing.T) {
+
+}
+
+func TestUpdate(t *testing.T) {
+
+}
+
+func TestDelete(t *testing.T) {
+
+}
